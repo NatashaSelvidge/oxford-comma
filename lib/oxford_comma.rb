872c707d6231a4cb41fe43985a84d4eb5aa5cb10
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-11-12 17:55:38 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+  if array.length == 1
+      return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+    array[-1].prepend ("and " )
+    return array.join( ", " )
+  elsif array.length >= 4
+    array[-1].prepend ("and " )
+    return array.join( ", " )
+  else
+    end
+  end
+  
+  
 
-end
+
+ # array << ["durian"].join("and")
+  #return array
+#else
